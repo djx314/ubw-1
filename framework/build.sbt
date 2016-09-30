e48aecb43ad520273da25a483563210a1ff01c92
djx314 <djx314@sina.cn>
2016-10-01 02:56:12 +0800

    Replace file path.

--- framework/build.sbt
+++ framework/build.sbt
@@ -4,24 +4,15 @@ import org.slf4j.LoggerFactory
 libraryDependencies += "com.lihaoyi" % "ammonite" % "0.7.7" % "test" cross CrossVersion.full
 
 if (scala.util.Properties.isWin)
-  initialCommands in (Test, console) += s"""ammonite.repl.Main.run("repl.frontEnd() = ammonite.repl.frontend.FrontEnd.JLineWindows");"""
+  initialCommands in (Test, console) += s"""ammonite.repl.Main.run("repl.frontEnd() = ammonite.repl.frontend.FrontEnd.JLineWindows");""" //Node that it doesn't work in ammonite 0.7.7
 else
   initialCommands in (Test, console) += s"""ammonite.Main().run();"""
+
+CustomSettings.baseSettings
 
 lazy val logger = {
   LoggerFactory.getLogger("sbt init")
 }
-
-lazy val modules = (project in file("./old/modules"))
-  .settings(CustomSettings.baseSettings: _*)
-  .settings(
-    name := "fsn-modules",
-    addCompilerPlugin(
-      "org.scalamacros" % "paradise" % "2.1.0" cross CrossVersion.full
-    )
-  )
-  .dependsOn(core)
-  .aggregate(core)
 
 lazy val core = (project in file("./old/fsn-core"))
   .settings(CustomSettings.baseSettings: _*)
---
+++ framework/project/CustomSettings.scala
@@ -0,0 +1,62 @@
+package net.scalax.sbt
+
+import sbt._
+import sbt.Keys._
+
+object CustomSettings {
+  
+  def customSettings = scalaSettings ++ resolversSettings ++ extAlias ++ projectVersion
+
+  def baseSettings = projectVersion ++ scalaSettings
+
+  def projectVersion = Seq(
+    organization := "net.scalax",
+    version := "0.0.3-M3"
+  )
+
+  def scalaSettings =
+    Seq(
+      scalaVersion := "2.11.8",
+      scalacOptions ++= Seq("-feature", "-deprecation")
+    )
+  
+  def resolversSettings =
+    Seq(
+      resolvers ++= Seq(
+        "mavenRepoJX" at "http://repo1.maven.org/maven2/",
+        "bintray/non" at "http://dl.bintray.com/non/maven",
+        Resolver.sonatypeRepo("release"),
+        Resolver.url("typesafe-ivy", url("http://repo.typesafe.com/typesafe/ivy-releases/"))(Resolver.ivyStylePatterns)
+      ),
+      externalResolvers := Resolver.withDefaultResolvers(resolvers.value, mavenCentral = false)
+    )
+  
+  def extAliasInfo = List(
+    Option("xeclipse" -> "eclipse with-source=true skip-parents=false"),
+    if (scala.util.Properties.isWin)
+      Option(windowsGitInitCommandMap)
+    else
+      Option(linuxGitInitCommandMap)
+  )
+
+  def extAlias = extAliasInfo.collect { case Some(s) => s }
+    .foldLeft(List.empty[Def.Setting[_]]){ (s, t) => s ++ addCommandAlias(t._1, t._2) }
+
+  //git init command
+  lazy val windowsGitInitCommandMap = "windowsGitInit" ->
+    """|;
+      |git config --global i18n.commitencoding utf-8;
+      |git config --global i18n.logoutputencoding gbk;
+      |git config --global core.autocrlf true;
+      |git config core.editor \"extras/commit_notepad.bat\"
+    """.stripMargin
+
+  lazy val linuxGitInitCommandMap = "linuxGitInit" ->
+    """|;
+      |git config --global i18n.commitencoding utf-8;
+      |git config --global i18n.logoutputencoding utf-8;
+      |git config --global core.autocrlf true;
+      |git config core.editor gedit
+    """.stripMargin
+
+}
