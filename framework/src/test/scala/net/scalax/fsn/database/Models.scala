4ec465fa6faf68b7b089b4e06e433725e5947f5c
djx314 <djx314@sina.cn>
2016-10-04 02:54:46 +0800

    coverage test.

---
+++ framework/src/test/scala/net/scalax/fsn/database/Models.scala
@@ -0,0 +1,20 @@
+package net.scalax.fsn.database.test
+
+import slick.jdbc.H2Profile.api._
+/**
+  * Created by djx314 on 15-6-22.
+  */
+
+case class Friend(
+  id: Option[Long],
+  name: String,
+  nick: String
+)
+
+class FriendTable(tag: Tag) extends Table[Friend](tag, "firend") {
+  def id = column[Long]("id", O.AutoInc)
+  def name = column[String]("name")
+  def nick = column[String]("nick")
+
+  def * = (id.?, name, nick).mapTo[Friend]
+}
