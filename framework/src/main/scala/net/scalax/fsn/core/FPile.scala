79594cd48c6f637a2c062054cd016b1d69c2b119
djx314 <djx314@sina.cn>
2016-11-17 23:17:09 +0800

    New API.

--- framework/src/main/scala/net/scalax/fsn/core/FPile.scala
+++ framework/src/main/scala/net/scalax/fsn/core/FPile.scala
@@ -1,9 +1,26 @@
 package net.scalax.fsn.core
 
-trait FPile {
+trait AbstractFPile {
 
   val columns: List[FColumn]
-  val transform: List[FPile] => FPile
-  val sourcePiles: List[FPile]
+
+}
+
+trait FPile[E, U] extends AbstractFPile {
+
+  val shape: FsnShape[E, U]
+
+}
+
+trait FsnShape[Packed_, UnPacked_] {
+
+  type Packed = Packed_
+  type UnPacked = UnPacked_
+
+  def encodeColumn(pile: Packed_): List[FColumn]
+  def decodeColumn(columns: List[FColumn]): Packed_
+
+  def encodeData(pileData: UnPacked_): List[Any]
+  def decodeData(data: List[Any]): UnPacked_
 
 }
