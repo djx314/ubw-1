112ba620ccd1a9f124c8b287eb3f8187333fe3a3
djx314 <djx314@sina.cn>
2016-11-21 23:13:04 +0800

    New atomic api.

--- framework/src/main/scala/net/scalax/fsn/core/FAtomicQuery.scala
+++ framework/src/main/scala/net/scalax/fsn/core/FAtomicQuery.scala
@@ -1,8 +1,9 @@
 package net.scalax.fsn.core
 
 import scala.language.higherKinds
+import scala.language.existentials
 
-trait FAtomicQuery[E[_], U[_]] {
+trait FAtomicQuery[E, U[_]] {
 
   val rep: E
 
@@ -16,29 +17,33 @@ trait FAtomicQuery[E[_], U[_]] {
 
 object FAtomicQuery {
 
-  def apply[E, D, U](rep1: E)(shape: FAtomicShape[E, D, U]): FAtomicQuery[E, D, U] = new FAtomicQuery[E, D, U] {
+  def apply[E, U[_]](rep1: E)(implicit shape: FAtomicShape[E, U]): FAtomicQuery[E, U] = new FAtomicQuery[E, U] {
     override val rep = rep1
     override val atomicSape = shape
   }
 
 }
 
-trait FAtomicShape[E[_], U[_]] {
+trait FAtomicShape[-E, U[_]] {
 
-  def unwrap[D](rep: E[D]): List[AbstractFAtomicGen]
+  def needWrapLength: Int
+  def unwrap(rep: E): List[AbstractFAtomicGen]
   def wrap[D](atomics: List[Any]): U[D]
 
 }
 
-object FAtomicShape {
+trait FAtomicShapeImpl {
 
   import shapeless._
 
-  implicit val hNilHListAtomicShape: FAtomicShape[HNil, HNil] = {
-    new FAtomicShape[HNil, HNil] {
+  type FNil[_] = HNil
 
-      def unwrap(rep: HNil) = Nil
-      def wrap(atomics: List[Any]) = HNil
+  implicit val hNilHListAtomicShape: FAtomicShape[HNil, FNil] = {
+    new FAtomicShape[HNil, FNil] {
+
+      override def needWrapLength = 0
+      override def unwrap(rep: HNil): List[AbstractFAtomicGen] = Nil
+      override def wrap[D](atomics: List[Any]): FNil[D] = HNil
 
     }
   }
@@ -46,23 +51,50 @@ object FAtomicShape {
   implicit def repLikeAtomicShape[S[_]]: FAtomicShape[FAtomicGen[S], S] = {
     new FAtomicShape[FAtomicGen[S], S] {
 
-      def unwrap(rep: FAtomicGen[S]) = rep :: Nil
-      def wrap(atomics: List[Any]) = atomics(0).asInstanceOf[S]
+      override def needWrapLength = 1
+      override def unwrap(rep: FAtomicGen[S]): List[AbstractFAtomicGen] = rep :: Nil
+      override def wrap[D](atomics: List[Any]): S[D] = atomics.head.asInstanceOf[S[D]]
 
     }
   }
 
-  implicit def hListAtomicShape[S <: HList, T <: HList, U, V, A <: HList, B <: HList]
-  (implicit repConvert: S <:< (U :: A), atomicConvert: T <:< (V :: B), subShape: FAtomicShape[U, V], tailShape: FAtomicShape[A, B]): FAtomicShape[S, T] = {
-    new FAtomicShape[S, T] {
+  /*implicit def hListAtomicShape[S, T[_], A <: HList, B[I] <: HList forSome { type I }]
+  (implicit subShape: FAtomicShape[S, T], tailShape: FAtomicShape[A, B]): FAtomicShape[S :: A, (T[H] :: B[H])] forSome { type H } = {
 
-      def unwrap(rep: S) = {
-        val subRep :: tailRep :: HNil = repConvert(rep)
+    type E[K] = T[K] :: B[K]
+
+    new FAtomicShape[S :: A, E] {
+
+      override def needWrapLength = subShape.needWrapLength + tailShape.needWrapLength
+
+      override def unwrap(rep: S :: A): List[AbstractFAtomicGen] = {
+        val subRep :: tailRep = rep
         subShape.unwrap(subRep) ::: tailShape.unwrap(tailRep)
       }
-      def wrap(atomics: List[Any]) = { ??? }
+      override def wrap[D](atomics: List[Any]): E[D] = {
+        (subShape.wrap(atomics.take(subShape.needWrapLength)): T[D]) :: (tailShape.wrap(atomics.drop(tailShape.needWrapLength)): B[D])
+      }
+,  forSome { type H }
+    }
+  }*/
+
+  /*implicit def hListAtomicShape[S <: HList, T[_], U, V[_], A <: HList, B[H] <: HList]
+  (implicit repConvert: S <:< (U :: A), subShape: FAtomicShape[U, V], tailShape: FAtomicShape[A, B]): FAtomicShape[S, J] = {
+    new FAtomicShape[S, J] {
+
+      override def needWrapLength = subShape.needWrapLength + tailShape.needWrapLength
+
+      override def unwrap(rep: S): List[AbstractFAtomicGen] = {
+        val subRep :: tailRep = repConvert(rep)
+        subShape.unwrap(subRep) ::: tailShape.unwrap(tailRep)
+      }
+
+      override def wrap[D](atomics: List[Any]): J[D] = {
+        //val aa = atomicConvert.asInstanceOf[(V[D] :: B[D]) <:< T[D]]
+        (((subShape.wrap(atomics.take(subShape.needWrapLength)): V[D]) :: ((tailShape.wrap(atomics.drop(tailShape.needWrapLength))): B[D])))
+      }
 
     }
-  }
+  }*/
 
 }
--- framework/src/test/scala/net/scalax/fsn/database/ParTest.scala
+++ framework/src/test/scala/net/scalax/fsn/database/ParTest.scala
@@ -1,10 +1,11 @@
 package net.scalax.fsn.database.test
 
-import net.scalax.fsn.core.{AbstractFAtomicGen, FAtomicGen, FAtomicGenImpl, FPathImpl}
+import net.scalax.fsn.core._
 import org.scalatest._
 import org.scalatest.concurrent.ScalaFutures
-import net.scalax.fsn.json.atomic.JsonReader
+import net.scalax.fsn.json.atomic.{JsonReader, JsonWriter}
 import net.scalax.fsn.mix.helpers.In
+import shapeless._
 
 class ParTest extends FlatSpec
   with Matchers
@@ -12,13 +13,21 @@ class ParTest extends FlatSpec
   with ScalaFutures
   with BeforeAndAfterAll
   with BeforeAndAfter
-  with FAtomicGenImpl {
+  with FAtomicGenImpl
+  with FAtomicShapeImpl {
 
   "shapes" should "find readers in Atomic in FPath" in {
     val path = FPathImpl(In.jRead[Long] ::: In.jWrite[Long])
     val bb: FAtomicGen[JsonReader] = needAtomic[JsonReader]
     val jsonReaderGen: AbstractFAtomicGen = needAtomic[JsonReader]
+
     println(bb.gen(path.atomics).reader)
+    println(FAtomicQuery(HNil).gen(path.atomics))
+    println(FAtomicQuery(needAtomic[JsonReader]).gen(path.atomics).reader)
+
+    //val (jsonReader1 :: jsonWriter1 :: HNil) = FAtomicQuery(needAtomic[JsonReader] :: needAtomic[JsonWriter] :: HNil)(hListAtomicShape).gen(path.atomics).aa
+    //println(jsonWriter1.writer)
+
   }
 
 }
