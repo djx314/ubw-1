f2c66e10f8969edb789e4099fbda36beca1bc776
djx314 <djx314@sina.cn>
2016-11-22 23:26:58 +0800

    Fix a bug in FAtmicQuery api.

--- framework/src/main/scala/net/scalax/fsn/core/FAtomicQuery.scala
+++ framework/src/main/scala/net/scalax/fsn/core/FAtomicQuery.scala
@@ -3,13 +3,19 @@ package net.scalax.fsn.core
 import scala.language.higherKinds
 import scala.language.existentials
 
-trait FAtomicQuery[E, U[_]] {
+trait AbstractFAtomicQuery[F[_]] {
+
+  def gen[D](atomics: List[FAtomic[D]]): F[D]
+
+}
+
+trait FAtomicQuery[E, F[_]] extends AbstractFAtomicQuery[F] {
 
   val rep: E
 
-  val atomicSape: FAtomicShape[E, U]
+  val atomicSape: FAtomicShape[E] { type U[K] = F[K] }
 
-  def gen[D](atomics: List[FAtomic[D]]): U[D] = {
+  override def gen[D](atomics: List[FAtomic[D]]): F[D] = {
     atomicSape.wrap(atomicSape.unwrap(rep).map { s => s.gen(atomics) })
   }
 
@@ -17,16 +23,18 @@ trait FAtomicQuery[E, U[_]] {
 
 object FAtomicQuery {
 
-  def apply[E, U[_]](rep1: E)(implicit shape: FAtomicShape[E, U]): FAtomicQuery[E, U] = new FAtomicQuery[E, U] {
+  def apply[E](rep1: E)(implicit shape: FAtomicShape[E]): FAtomicQuery[E, shape.U] = new FAtomicQuery[E, shape.U] {
     override val rep = rep1
-    override val atomicSape = shape
+    override val atomicSape  = shape: FAtomicShape[E] { type U[K] = shape.U[K] }
   }
 
 }
 
-trait FAtomicShape[-E, U[_]] {
+trait FAtomicShape[-E] {
 
-  def needWrapLength: Int
+  type U[_]
+
+  val needWrapLength: Int
   def unwrap(rep: E): List[AbstractFAtomicGen]
   def wrap[D](atomics: List[Any]): U[D]
 
@@ -38,63 +46,45 @@ trait FAtomicShapeImpl {
 
   type FNil[_] = HNil
 
-  implicit val hNilHListAtomicShape: FAtomicShape[HNil, FNil] = {
-    new FAtomicShape[HNil, FNil] {
+  implicit val hNilHListAtomicShape = {
+    new FAtomicShape[HNil] {
 
-      override def needWrapLength = 0
+      override type U[_] = HNil
+
+      override val needWrapLength = 0
       override def unwrap(rep: HNil): List[AbstractFAtomicGen] = Nil
-      override def wrap[D](atomics: List[Any]): FNil[D] = HNil
+      override def wrap[D](atomics: List[Any]): HNil = HNil
 
     }
   }
 
-  implicit def repLikeAtomicShape[S[_]]: FAtomicShape[FAtomicGen[S], S] = {
-    new FAtomicShape[FAtomicGen[S], S] {
+  implicit def repLikeAtomicShape[S[_]] = {
+    new FAtomicShape[FAtomicGen[S]] {
 
-      override def needWrapLength = 1
+      override type U[K] = S[K]
+
+      override val needWrapLength = 1
       override def unwrap(rep: FAtomicGen[S]): List[AbstractFAtomicGen] = rep :: Nil
       override def wrap[D](atomics: List[Any]): S[D] = atomics.head.asInstanceOf[S[D]]
 
     }
   }
 
-  /*implicit def hListAtomicShape[S, T[_], A <: HList, B[I] <: HList forSome { type I }]
-  (implicit subShape: FAtomicShape[S, T], tailShape: FAtomicShape[A, B]): FAtomicShape[S :: A, (T[H] :: B[H])] forSome { type H } = {
+  implicit def hListAtomicShape[S <: HList, E, A <: HList]
+  (implicit repConvert: S <:< (E :: A), subShape: FAtomicShape[E], tailShape: FAtomicShape[A] { type U[K] <: HList }) = {
+    new FAtomicShape[S] {
 
-    type E[K] = T[K] :: B[K]
+      override type U[K] = subShape.U[K] :: tailShape.U[K]
 
-    new FAtomicShape[S :: A, E] {
-
-      override def needWrapLength = subShape.needWrapLength + tailShape.needWrapLength
-
-      override def unwrap(rep: S :: A): List[AbstractFAtomicGen] = {
-        val subRep :: tailRep = rep
-        subShape.unwrap(subRep) ::: tailShape.unwrap(tailRep)
-      }
-      override def wrap[D](atomics: List[Any]): E[D] = {
-        (subShape.wrap(atomics.take(subShape.needWrapLength)): T[D]) :: (tailShape.wrap(atomics.drop(tailShape.needWrapLength)): B[D])
-      }
-,  forSome { type H }
-    }
-  }*/
-
-  implicit def hListAtomicShape[S <: HList, T[_] <: HList, U, V[_], A <: HList, B[H] <: HList]
-  (implicit repConvert: S <:< (U :: A), subShape: FAtomicShape[U, V], tailShape: FAtomicShape[A, B]) = {
-    type C[K] = V[K] :: B[K]
-    new FAtomicShape[S, C] {
-
-      override def needWrapLength = subShape.needWrapLength + tailShape.needWrapLength
+      override val needWrapLength = subShape.needWrapLength + tailShape.needWrapLength
 
       override def unwrap(rep: S): List[AbstractFAtomicGen] = {
         val subRep :: tailRep = repConvert(rep)
         subShape.unwrap(subRep) ::: tailShape.unwrap(tailRep)
       }
 
-      override def wrap[D](atomics: List[Any]): C[D] = {
-        //val aa = atomicConvert.asInstanceOf[(V[D] :: B[D]) <:< T[D]]
-        println(atomics.take(subShape.needWrapLength))
-        println(tailShape.wrap(atomics.drop(subShape.needWrapLength)))
-        (((subShape.wrap(atomics.take(subShape.needWrapLength)): V[D]) :: ((tailShape.wrap(atomics.drop(subShape.needWrapLength))): B[D])))
+      override def wrap[D](atomics: List[Any]): subShape.U[D] :: tailShape.U[D] = {
+        (subShape.wrap(atomics.take(subShape.needWrapLength)): subShape.U[D]) :: (tailShape.wrap(atomics.drop(subShape.needWrapLength)): tailShape.U[D])
       }
 
     }
