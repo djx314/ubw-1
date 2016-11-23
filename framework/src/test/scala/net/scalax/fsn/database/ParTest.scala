376d5da0e97dfcfd0b469bfec31efa1d62c2c6c8
djx314 <djx314@sina.cn>
2016-11-24 00:49:54 +0800

    Some fix.

--- framework/src/test/scala/net/scalax/fsn/database/ParTest.scala
+++ framework/src/test/scala/net/scalax/fsn/database/ParTest.scala
@@ -29,7 +29,7 @@ class ParTest extends FlatSpec
     println(FAtomicQuery(needAtomic[JsonReader] :: needAtomic[JsonReader] :: needAtomic[JsonWriter] :: needAtomic[JsonWriter] :: HNil).gen(path.atomics))
 
     val items: List[FAtomic[Long]] = path.atomics
-    val reader1 :: reader2 :: writer1 :: HNil = FAtomicQuery(needAtomic[JsonReader] :: needAtomic[JsonReader] :: needAtomic[JsonWriter] :: HNil).gen(path.atomics)
+    val reader1 :: reader2 :: writer1 :: HNil = FAtomicQuery(needAtomic[JsonReader] :: needAtomic[JsonReader] :: needAtomic[JsonWriter] :: HNil).gen(items)
     println(reader2.reader)
     println(writer1.writer)
     println(FAtomicQuery(needAtomic[JsonReader] :: needAtomic[JsonReader] :: needAtomic[JsonWriter] :: HNil).gen(path.atomics)(2).writer)
