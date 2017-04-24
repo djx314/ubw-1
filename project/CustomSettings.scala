a570140aa8fcf7d3c4b0337285176218b52eada6
djx314 <djx314@sina.cn>
2017-04-24 17:28:22 +0800

    Remove error scalac option.

--- project/CustomSettings.scala
+++ project/CustomSettings.scala
@@ -16,7 +16,7 @@ object CustomSettings {
   def scalaSettings =
     Seq(
       scalaVersion := "2.12.2",
-      scalacOptions ++= Seq("-feature", "-deprecation", "-Ywarn-unused:-import")
+      scalacOptions ++= Seq("-feature", "-deprecation")
     )
   
   def resolversSettings =
