fb75fcd19c41da3ab123d786ba81357c784d6c8a
djx314 <djx314@sina.cn>
2017-05-01 12:59:12 +0800

    Add new sample.

--- sample/commonSlick/src/main/scala/Sample07.scala
+++ sample/commonSlick/src/main/scala/Sample07.scala
@@ -3,19 +3,20 @@ package net.scalax.fsn.database.test
 import io.circe.{ Json, Printer }
 import io.circe.syntax._
 import io.circe.generic.auto._
-import net.scalax.fsn.core.FPathImpl
+import net.scalax.fsn.core.{ FPathImpl, PilesPolyHelper }
 import net.scalax.fsn.json.operation.{ FDefaultAtomicHelper, FPropertyAtomicHelper }
-import net.scalax.fsn.mix.helpers.{ Slick2JsonFsnImplicit, SlickCRUDImplicits }
+import net.scalax.fsn.mix.helpers.{ In, Slick2JsonFsnImplicit, SlickCRUDImplicits }
 import net.scalax.fsn.slick.helpers.{ FJsonAtomicHelper, FSelectExtAtomicHelper, FStrSelectExtAtomicHelper, StrFSSelectAtomicHelper }
 import net.scalax.fsn.slick.model._
 
 import scala.concurrent.ExecutionContext.Implicits.global
 import scala.language.implicitConversions
 import slick.jdbc.H2Profile.api._
+import shapeless._
 
 import scala.concurrent._
 
-object Sample06 extends SlickCRUDImplicits with StrFSSelectAtomicHelper with Slick2JsonFsnImplicit {
+object Sample07 extends SlickCRUDImplicits with StrFSSelectAtomicHelper with Slick2JsonFsnImplicit with PilesPolyHelper with App {
 
   val printer = Printer(
     preserveOrder = true,
@@ -48,9 +49,22 @@ object Sample06 extends SlickCRUDImplicits with StrFSSelectAtomicHelper with Sli
     friend <- FriendTable.out
   } yield {
     List(
-      "id" ofPile friend.id.out.order.describe("自增主键").inView(false).writeJ,
-      "name" ofPile friend.name.out.orderTarget("nick").describe("昵称").writeJ,
-      "nick" ofPile friend.nick.out.order.describe("昵称").inView(false).writeJ,
+      "id" ofPile friend.id.out.order.describe("自增主键").writeJ,
+      (
+        ("name" ofPile friend.name.out.orderTarget("nick").describe("昵称")) ::
+        ("nick" ofPile friend.nick.out.order.describe("昵称")) ::
+        ("age" ofPile friend.age.out) ::
+        HNil
+      ).poly(
+          "name" ofPath FPathImpl.empty[String].writeJ
+        ).apply {
+            case Some(name) :: Some(nick) :: Some(Some(age)) :: HNil if age < 200 =>
+              Option(s"$name-$nick")
+            case Some(name) :: _ :: _ :: HNil =>
+              Option(name)
+            case _ =>
+              None
+          },
       "ageOpt" ofPile friend.age.out.writeJ
     )
   }
