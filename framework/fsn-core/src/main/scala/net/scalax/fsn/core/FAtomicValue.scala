ddb562e3ef558e08f613ad2125c3b017ec415787
djx314 <djx314@sina.cn>
2017-05-12 15:08:40 +0800

    Some fix.

--- framework/fsn-core/src/main/scala/net/scalax/fsn/core/FAtomicValue.scala
+++ framework/fsn-core/src/main/scala/net/scalax/fsn/core/FAtomicValue.scala
@@ -1,44 +1,28 @@
 package net.scalax.fsn.core
 
-trait FAtomicValue extends FAtomicWrap
+trait FAtomicValue {
+  type DataType
+  val atomic: Option[FAtomic[DataType]]
+}
 
 trait FAtomicValueImpl[D] extends FAtomicValue {
   self =>
-  override val atomics: List[FAtomic[D]]
+  override val atomic: Option[FAtomic[D]]
   override type DataType = D
-
-  def append(path: FAtomicPathImpl[D]): FAtomicPathImpl[D] = new FAtomicPathImpl[D] {
-    override val atomics = self.atomics ::: path.atomics
-  }
-
-  def appendAll(path: FAtomicPathImpl[D]*): FAtomicPathImpl[D] = new FAtomicPathImpl[D] {
-    override val atomics = self.atomics ::: path.toList.flatMap(_.atomics)
-  }
-
-  def appendAtomic(atomic: FAtomic[D]): FAtomicPathImpl[D] = new FAtomicPathImpl[D] {
-    override val atomics = self.atomics ::: atomic :: Nil
-  }
-
-  def appendAllAtomic(atomics: FAtomic[D]*): FAtomicPathImpl[D] = {
-    val atomics1 = atomics
-    new FAtomicPathImpl[D] {
-      override val atomics = self.atomics ::: atomics1.toList
-    }
-  }
 }
 
 object FAtomicValueImpl {
 
-  def apply[D](atomics: List[FAtomic[D]]): FAtomicValueImpl[D] = {
+  def apply[D](atomics: Option[FAtomic[D]]): FAtomicValueImpl[D] = {
     val atomics1 = atomics
     new FAtomicValueImpl[D] {
-      override val atomics = atomics1
+      override val atomic = atomics1
     }
   }
 
   def empty[D]: FAtomicValueImpl[D] = {
     new FAtomicValueImpl[D] {
-      override val atomics = Nil
+      override val atomic = None
     }
   }
 
