cf403f844327a77e690bb3932b879a8cb5330afc
djx314 <djx314@sina.cn>
2017-05-30 04:49:25 +0800

    Add unapply.

--- sample/commonSlick/src/main/scala/Sample07.scala
+++ sample/commonSlick/src/main/scala/Sample07.scala
@@ -1,13 +1,14 @@
 package net.scalax.fsn.database.test
 
-import net.scalax.fsn.core.{ FAtomicPathImpl, PilesPolyHelper }
-import net.scalax.fsn.json.operation.{ FAtomicValueHelper, FDefaultAtomicHelper, FPropertyAtomicHelper }
+import net.scalax.fsn.core.{ FAtomicPathImpl, FAtomicValueImpl, PilesPolyHelper }
+import net.scalax.fsn.json.operation._
 import net.scalax.fsn.mix.helpers.{ Slick2JsonFsnImplicit, SlickCRUDImplicits }
 import net.scalax.fsn.slick.helpers.{ FJsonAtomicHelper, FStrSelectExtAtomicHelper, StrFSSelectAtomicHelper }
 import net.scalax.fsn.slick.model._
 import io.circe.Json
 import io.circe.generic.auto._
 import io.circe.syntax._
+import net.scalax.fsn.common.atomic.FValue
 
 import scala.concurrent.ExecutionContext.Implicits.global
 import scala.language.implicitConversions
@@ -43,14 +44,11 @@ object Sample07 extends SlickCRUDImplicits
       ).poly(
           "name" ofPile FAtomicPathImpl.empty[String].writeJ
         ).transform {
-            case nameAt :: nickAt :: ageAt :: HNil if ageAt.opt.flatten.map(_ < 200).getOrElse(false) =>
-              for {
-                name <- nameAt
-                nick <- nickAt
-              } yield s"${name}-${nick}"
-            case name :: _ :: _ :: HNil if name.isDefined =>
+            case FSomeValue(name) :: FSomeValue(nick) :: FSomeValue(Some(age)) :: HNil if age < 200 =>
+              set(s"${name}-${nick}")
+            case (name @ FSomeValue(_)) :: FSomeValue(_) :: FSomeValue(_) :: HNil =>
               name
-            case s =>
+            case _ =>
               emptyValue[String]
           },
       "ageOpt" ofPile friend.age.out.writeJ
@@ -85,22 +83,22 @@ object Sample07 extends SlickCRUDImplicits
       ).poly(
           "name" ofPile FAtomicPathImpl.empty[String]
         ).transform {
-            case name :: nick :: age :: HNil if (name.isDefined && nick.isDefined && age.opt.flatten.isDefined) && age.opt.flatten.get < 200 =>
-              set(s"${name.get}-${nick.get}")
-            case name :: _ :: _ :: HNil if name.isDefined =>
-              set(name.get)
+            case FSomeValue(name) :: FSomeValue(nick) :: FSomeValue(Some(age)) :: HNil if age < 200 =>
+              set(s"${name}-${nick}")
+            case FSomeValue(name) :: _ :: _ :: HNil =>
+              set(name)
             case _ =>
               emptyValue[String]
           }) :: ("ageOpt" ofPile friend.age.out) :: FPNil).poly("account" ofPile FAtomicPathImpl.empty[Aa]).transform {
-            case name :: age :: HNil if name.isDefined && age.opt.flatten.isDefined =>
-              set(Aa(name.get, age.opt.flatten.get))
+            case FSomeValue(name) :: FSomeValue(Some(age)) :: HNil =>
+              set(Aa(name, age))
             case _ =>
               emptyValue[Aa]
           } :: ("id" ofPile friend.id.out.order.describe("自增主键")) :: ("id" ofPile friend.age.out.order.describe("年龄")) :: FPNil).poly("info" ofPile FAtomicPathImpl.empty[Map[String, Json]].writeJ).transform {
-            case aa :: id :: ageOpt :: HNil if aa.isDefined && id.isDefined && ageOpt.isDefined =>
-              set(Map("id" -> id.get.asJson, "accountInfo" -> aa.get.asJson, "ageOpt" -> ageOpt.get.asJson))
-            case _ :: id :: _ :: HNil if id.isDefined =>
-              set(Map("message" -> s"id为${id.get}的不知名人事".asJson))
+            case FSomeValue(aa) :: FSomeValue(id) :: FSomeValue(ageOpt) :: HNil =>
+              set(Map("id" -> id.asJson, "accountInfo" -> aa.asJson, "ageOpt" -> ageOpt.asJson))
+            case _ :: FSomeValue(id) :: _ :: HNil =>
+              set(Map("message" -> s"id为${id}的不知名人事".asJson))
           },
       "ageOpt" ofPile friend.age.out.writeJ
     )
