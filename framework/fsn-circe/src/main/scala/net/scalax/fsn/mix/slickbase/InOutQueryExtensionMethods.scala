8d78c4009a82a6291c69b00b9eb1452b6b3e9bb2
djx314 <djx314@sina.cn>
2017-06-02 16:49:01 +0800

    Add inOutQuery.

---
+++ framework/fsn-circe/src/main/scala/net/scalax/fsn/mix/slickbase/InOutQueryExtensionMethods.scala
@@ -0,0 +1,63 @@
+package net.scalax.fsn.mix.slickbase
+
+import net.scalax.fsn.core.FPile
+import net.scalax.fsn.slick.helpers.SlickQueryBindImpl
+import slick.ast.{ AnonSymbol, Ref }
+import slick.lifted._
+
+import scala.concurrent.ExecutionContext
+
+case class InOutQueryWrap(
+  columns: List[FPile],
+  crudBinds: List[(Any, SlickQueryBindImpl)],
+  listQueryBind: SlickQueryBindImpl
+)(implicit val ec: ExecutionContext)
+
+class InOutQueryExtensionMethods[E, U](val queryToExt: Query[E, U, Seq]) {
+
+  def flatMap[A, B](f: E => InOutQueryWrap): InOutQueryWrap = {
+    val generator = new AnonSymbol
+    val aliased = queryToExt.shaped.encodeRef(Ref(generator)).value
+    val fv = f(aliased)
+    val slickJsonQuery = new SlickQueryBindImpl {
+      override def bind[E, U](query: Query[E, U, Seq]): Query[E, U, Seq] = {
+        val newQuery = fv.listQueryBind.bind(query)
+        new WrappingQuery[E, U, Seq](new slick.ast.Bind(generator, queryToExt.toNode, newQuery.toNode), newQuery.shaped)
+      }
+    }
+    InOutQueryWrap(fv.columns, fv.crudBinds, slickJsonQuery)(fv.ec)
+  }
+
+  def map(ev: E => FQueryWrap)(implicit ec: ExecutionContext): InOutQueryWrap = {
+    val generator = new AnonSymbol
+    val aliased = queryToExt.shaped.encodeRef(Ref(generator)).value
+    val columns = ev(aliased)
+    val slickJsonQuery = new SlickQueryBindImpl {
+      override def bind[E, U](query: Query[E, U, Seq]): Query[E, U, Seq] = {
+        new WrappingQuery[E, U, Seq](new slick.ast.Bind(generator, queryToExt.toNode, query.toNode), query.shaped)
+      }
+    }
+    InOutQueryWrap(columns.columns, columns.binds, slickJsonQuery)(ec)
+  }
+
+  def filter[T <: Rep[_]: CanBeQueryCondition](f: E => T): PileListQueryExtensionMethods[E, U] = {
+    val cv = implicitly[CanBeQueryCondition[T]]
+    new PileListQueryExtensionMethods(queryToExt.filter(f)(cv))
+  }
+
+  def withFilter[T: CanBeQueryCondition](f: E => T): PileListQueryExtensionMethods[E, U] = {
+    val cv = implicitly[CanBeQueryCondition[T]]
+    new PileListQueryExtensionMethods(queryToExt.withFilter(f)(cv))
+  }
+
+  def filterNot[T <: Rep[_]: CanBeQueryCondition](f: E => T): PileListQueryExtensionMethods[E, U] = {
+    val cv = implicitly[CanBeQueryCondition[T]]
+    new PileListQueryExtensionMethods(queryToExt.filterNot(f)(cv))
+  }
+
+  def groupBy[K, T, G, P](f: E => K)(implicit kshape: Shape[_ <: FlatShapeLevel, K, T, G], vshape: Shape[_ <: FlatShapeLevel, E, _, P]): PileListQueryExtensionMethods[(G, Query[P, U, Seq]), (T, Query[P, U, Seq])] = {
+    val newQuery = queryToExt.groupBy(f)(kshape, vshape)
+    new PileListQueryExtensionMethods(newQuery)
+  }
+
+}
